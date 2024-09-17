cea1a97ebb5b4ed4f673f6b863736b59d7122978
Vrashank Shetty <vrashankshetty.s@gmail.com>
2024-09-17 17:08:24 +0530

    second

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,10 +1,10 @@
 import { Redis } from '@upstash/redis'
 import { Ratelimit } from "@upstash/ratelimit";
-import { UPTASH_TOKEN,UPTASH_URL } from '../../env';
+import '../../envConfig'
 
 export const redis = new Redis({
-  url: UPTASH_URL,
-  token: UPTASH_TOKEN,
+  url: process.env.UPTASH_URL,
+  token: process.env.UPTASH_TOKEN,
 })
 
 
